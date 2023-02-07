a412db3bce41c0f20b554d8da475c558bfb6ef1f
Vadim Barmenkov <vadimbarmenkov@gmail.com>
2023-02-07 15:03:09 +0300

    fixed 1

---
+++ src/js/Character.js
@@ -0,0 +1,26 @@
+export  class Character{
+    constructor(name) {
+        this.name = name;
+        this.stoned = false;
+    }
+
+    getAttack(numberOfCells){
+        if(this.stoned){
+            return this.attack - Math.log2(numberOfCells) * 5 - (this.attack * (numberOfCells - 1) * 0.1);
+        }else {
+            return this.attack - (this.attack * (numberOfCells - 1) * 0.1);
+        }
+    }
+
+    setAttack(attackPower){
+        this.attack = attackPower;
+    }
+
+    setStoned(stoned){
+        this.stoned = stoned;
+    }
+
+    getStoned(){
+        return this.stoned;
+    }
+}
--- src/js/Magician.js
+++ src/js/Magician.js
@@ -1,27 +1,8 @@
-export class Magician {
-    constructor(name) {
-        this.name = name;
+import {Character} from "./Character";
+
+export class Magician extends Character{
+    constructor(...args) {
+        super(args);
         this.type = 'Magician';
-        this.stoned = false;
-    }
-
-    getAttack(numberOfCells){
-        if(this.stoned){
-            return this.attack - Math.log2(numberOfCells) * 5 - (this.attack * (numberOfCells - 1) * 0.1);
-        }else {
-            return this.attack - (this.attack * (numberOfCells - 1) * 0.1);
-        }
-    }
-
-    setAttack(attackPower){
-        this.attack = attackPower;
-    }
-
-    setStoned(stoned){
-        this.stoned = stoned;
-    }
-
-    getStoned(){
-        return this.stoned;
     }
 }
